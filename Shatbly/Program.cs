26f80221b40f1a9746901506c4e189f66765c89a
Mohamed Sayed <mo.sayed2037@gmail.com>
2026-04-23 12:06:51 +0200

    Finish task 2

--- Shatbly/Program.cs
+++ Shatbly/Program.cs
@@ -40,7 +40,12 @@ namespace Shatbly
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddScoped<IDbintialize, Dbintialize>();
-
+            builder.Services.AddScoped<IRepository<User>, Repository<User>>();
+            builder.Services.AddScoped<IRepository<WorkerProfile>, Repository<WorkerProfile>>();
+            builder.Services.AddScoped<IRepository<Address>, Repository<Address>>();
+             builder.Services.AddScoped<IRepository<Booking>, Repository<Booking>>();
+             builder.Services.AddScoped<IRepository<Coupon>, Repository<Coupon>>();
+             builder.Services.AddScoped<IRepository<PromotionCode>, Repository<PromotionCode>>();
             builder.Services.AddScoped<IAccountService, Services.AccountService>();
 
             builder.Services.AddTransient<IEmailSender, EmailSender>();
